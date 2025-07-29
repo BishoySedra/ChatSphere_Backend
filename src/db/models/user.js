e7045ac487c7258e6d7f208f2669b6ce229dc4fd
Bishoy Sedra <bishosedra0@gmail.com>
2025-07-29 05:50:20 +0300

    Added image_url field to user model!

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -16,6 +16,9 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  image_url: {
+    type: String
+  },
   password: {
     type: String,
     required: true,
